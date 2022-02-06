62b244a469592c59722e8fffc8d69de0a31eb257
JoseXtu <99103269+JoseXtu@users.noreply.github.com>
2022-02-06 07:30:46 -0300

    Add files via upload

---
+++ assets/preload/custom_events/note fade or appear.lua
@@ -0,0 +1,12 @@
+function onEvent(name, value1, value2)
+    if name == 'note fade or appear' then
+            noteTweenAlpha("note yeah", 0, value1, 0.3, "linear");
+            noteTweenAlpha("LOL", 1, value1, 0.3, "linear");
+            noteTweenAlpha("why u lookin", 2, value1, 0.3, "linear");
+            noteTweenAlpha("maze", 3, value1, 0.3, "linear");
+            noteTweenAlpha("tord", 4, value2, 0.3, "linear");
+            noteTweenAlpha("is", 5, value2, 0.3, "linear");
+            noteTweenAlpha("a furry", 6, value2, 0.3, "linear");
+            noteTweenAlpha("red", 7, value2, 0.3, "linear");
+    end
+end
